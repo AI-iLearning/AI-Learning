dc933d6307c536863c9a356da6ffb9c6ce989df5
Mheejeong <mheejeong12@example.com>
2024-09-14 17:54:09 +0000

    Add Kakao login and token management views

--- backend/ailearning/models.py
+++ backend/ailearning/models.py
@@ -1,3 +1,10 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class UserProfile(AbstractUser):  # Django 기본 사용자 모델을 상속
+    kakao_id = models.CharField(max_length=100, unique=True, null=True, blank=True)
+    kakao_access_token = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
---
+++ backend/ailearning/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views.auth_views import kakao_callback
+from .views.token_views import kakao_new_access
+
+urlpatterns = [
+    path('members/kakao-callback/', kakao_callback, name='kakao-callback'),
+    path('members/kakao-new-access/', kakao_new_access, name='kakao-new-access'),
+]

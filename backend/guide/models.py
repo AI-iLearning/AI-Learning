0fec4b8ca426a432fa5888aca123229be5eaf11f
Mheejeong <mheejeong12@gmail.com>
2024-10-14 03:44:16 +0000

    가이드3

--- backend/guide/models.py
+++ backend/guide/models.py
@@ -4,12 +4,14 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+def get_default_user():
+    return User.objects.filter(username='default_user').first() or None
+
 class SendChat(models.Model):
     guide_id = models.IntegerField()
-    chat_message = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True, blank=True)
-
+    chat_message = models.TextField(default="")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, default=get_default_user)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.chat_message
